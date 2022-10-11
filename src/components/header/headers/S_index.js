8411cbdd5ff1ae73120aeab506493db3233dffeb
Al3ncar <alencar29htcss@gmail.com>
2022-10-11 14:42:41 -0300

    Atualizando a Header

--- src/components/header/headers/S_index.js
+++ src/components/header/headers/S_index.js
@@ -1,5 +1,10 @@
 import React from "react"
 import Nitro from "./img/nitroO.jpg"
+
+import Facelog from "./img/facelog.png"
+import Preflog from "./img/prefeituralog.png"
+import Twitt from "./img/twitterlog.png"
+
 import * as S from "./styled"
 
 export default function App(){
@@ -9,11 +14,27 @@ export default function App(){
                 <S.ContTitl>
                     <S.Titl>Cidade Nitro Operaria</S.Titl>
                 </S.ContTitl>
+                <S.BoxRedes>
+                    <S.Box href="https://pt-br.facebook.com/noticiassaomiguel/" target="_blank">
+                        <S.RedesImg src={Facelog} alt=""/>
+                    </S.Box>
+                    <S.Box href="https://twitter.com/sao_miguel" target="_blank">
+                        <S.RedesImg src={Twitt} alt=""/>
+                    </S.Box>
+                    <a href="https://www.prefeitura.sp.gov.br/cidade/secretarias/subprefeituras/sao_miguel_paulista/historico/index.php?p=432" target="_blank">
+                        <S.RedesImg src={Preflog} alt=""/>
+                    </a>
+                </S.BoxRedes>
                 <S.ContParg>
                     <S.Parg>
                         Fundada em 1936, a história da Nitro Química aposta de expansão do parque industrial após a crise de 1929 - se atrela à urbanização do quadricentenário bairro de São Miguel Paulista.
                     </S.Parg>
                 </S.ContParg>
+                <S.ContBtn>
+                    <S.Btn>
+                        <S.Ancs href="https://www.prefeitura.sp.gov.br/cidade/secretarias/subprefeituras/sao_miguel_paulista/historico/index.php?p=432" target="_blank">Seja mais sobre</S.Ancs>
+                    </S.Btn>
+                </S.ContBtn>
             </S.ContParf>
             <S.BoxImg>
                 <S.ImgFund src={Nitro} alt=""/>
--- src/components/header/headers/styled.js
+++ src/components/header/headers/styled.js
@@ -1,12 +1,16 @@
 import styled from "styled-components"
 
 export const ContNav = styled.div`
-    width:95%;
-    margin:1.5% 2.5%;
-    padding:0.5%;
+    width:75%;
     position:absolute;
-    background-color:darkblue;
-    z-index:5;
+    display:flex;
+    justify-content:space-between;
+    align-items: center;
+    padding:0.5%;
+    top:5%;
+    right: 8.5%;
+    background-color: darkblue;
+    border-radius: 2vw;
 `
 export const NavList = styled.nav`
     width: 100%;
@@ -14,21 +18,23 @@ export const NavList = styled.nav`
 export const List = styled.ul`
     width: 100%;
     display:flex;
-    justify-content:space-around;
+    justify-content:space-between;
     align-items: center;
+
 `
 export const Anc = styled.a`
     width:100%;
-    padding:1%;
+    padding:1% 0;
     color:white;
 
     text-align: center;
     list-style: none;
     font-weight: bolder;
-    font-size:1.3vw;
+    font-size:1vw;
     z-index:5;
     &:hover{
         outline: solid 0.2vw white;
+        border-radius: 2vw;
     }
 `
 
@@ -41,44 +47,94 @@ export const ContCity = styled.div`
 
 `
 export const ContParf = styled.div`
-    width:40%;
+    width:8%;
     height: 100%;
     display:flex;
     align-items:center;
     flex-direction:column;
-`
-export const ContTitl = styled.div`
-    width:100%;
-    margin-top:25%;
 
 `
+export const BoxRedes = styled.div`
+    width:40%;
+    z-index: 3;
+    display: grid;
+    place-items: center;
+    position: relative;
+    top:70%;
+`
+export const Box = styled.a`
+    width: 80%;
+    background-color: white;
+    border-radius: 50%;
+    display: grid;
+    margin:10%;
+`
+export const RedesImg = styled.img`
+    width:100%;
+    border-radius: 50%;
+`
+export const ContTitl = styled.div`width:10%;`
 export const Titl = styled.h1`
-    width: 60%;
+    width: 30%;
     color:white;
-    font-family: 'Sigmar One', cursive;
+    /* font-family: 'Sigmar One', cursive; */
 
     position: absolute;
     top:25%;
-    left: 3%;
-    font-size: 4.5vw;
+    right: 81.5%;
+    transform: rotate(270deg);
+    font-size: 2vw;
     z-index: 1;
 `
 export const ContParg = styled.div`
-    width:60%;
+    width: 100%;
     margin:35% 0 0 5%;
     color:white;
     font-weight: bolder;
-    font-size: 1.6vw;
+    font-size: 1.2vw;
     z-index: 1;
+    background-color: red;
 `
 export const Parg = styled.p`
-    width:60%;
-    padding:2%;
-    background-color:darkblue;
-    clip-path: polygon(0 0, 97% 0%, 100% 100%, 0 100%);
+    width:35%;
+    padding:2%; 
+    text-align:right;
     position:absolute;
-    top:73%;
-    left:3%;
+    top:70%;
+    right:8.5%;
+    background-color: darkblue;
+    border-radius: 2vw;
+    
+`
+export const ContBtn = styled.div`
+    width:15%;
+    position:absolute;
+    top:91%;
+    right:15%;
+`
+export const Btn = styled.button`
+    width:100%;
+    font-weight: bolder;
+    font-size:1.1vw;
+    padding: 2% 1%;
+    background-color: darkblue;
+    color:white;
+    border-radius: 2vw;
+    border: none;
+    transition:0.2s ease-in-out;
+    :hover{
+        transform:scale(1.1);
+        background-color:white;
+        color:darkblue;
+    }
+
+`
+export const Ancs = styled.a`
+    text-decoration:none;
+    color:white;
+    :hover{
+        color:darkblue;
+    }
 `
 export const BoxImg = styled.div`
     width:100%;
@@ -89,5 +145,4 @@ export const BoxImg = styled.div`
 export const ImgFund = styled.img`
     width:100%;
     height: 100%;
-    clip-path: polygon(0 0, 100% 0%, 100% 100%, 10% 100%);
 `
